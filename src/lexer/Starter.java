2bf66e06fd3492b7ec29f66c9c29efd8e3b2641d
bwotka <b.wotka@mail.de>
2020-07-01 17:09:19 +0200

    Starter erstellt, sonst nicht kompilierbar

---
+++ src/lexer/Starter.java
@@ -0,0 +1,9 @@
+package lexer;
+/** This class only exists because the normal classes wont be compiled without a main method.
+@author Benedikt Wotka
+ */
+public class Starter {
+    public static void main(String[] args){
+        System.out.println("All tokens compiled");
+    }
+}
